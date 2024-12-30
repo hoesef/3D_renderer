217ff2873d0b3a4473fdf8f5ee0e894b03ecb7d5
Joe <joe.luke.manning@gmail.com>
2024-12-30 19:18:10 +0000

    Updated run script

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,5 +1,5 @@
 @echo off
 cd C:\Users\joelu\Documents\Coding\3D_renderer
-g++ -Wall -Werror -o build\debug\main src\main.cpp src\core\framebuffer.cpp
+g++ -Wall -Werror -o build\debug\main src\main.cpp src\core\framebuffer.cpp src\core\linedrawer.cpp
 build\debug\main
 code image.ppm
